2355cbcd88f96d67f4c0f4b144fa639456e07a96
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-06-12 10:32:57 -0300

    "Melhorias de casos de teste e mod de de métodos"

--- IncluirLocal.py
+++ IncluirLocal.py
@@ -1,55 +1,50 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
-
-
+from selenium import webdriver
 
 def incluirLocal(_username, _password, unidadeTecnica, nomeLocal):
-    
-    try: 
-        #Instancia webdriver e chama link de gestor
+    try:
+        # Instancia webdriver e chama link de gestor
         driver = webdriver.Chrome()
         driver.implicitly_wait(10)
         driver.get("https://haishc2.famema.br")
         time.sleep(2)
-        #Entra passando credenciais
+
+        # Entra passando credenciais
         element = driver.find_element(By.ID, 'username')
         element.send_keys(_username)
         element = driver.find_element(By.ID, 'password')
         element.send_keys(_password)
         time.sleep(2)
+
         element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/form/button')
         element.send_keys(Keys.ENTER)
         time.sleep(5)
-        #Clica no botão incluir novo local
+
+        # Clica no botão incluir novo local
         element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/main/div[2]/div[1]/button')
         element.click()
         time.sleep(2)
-        #Seleciona unidade técnica hci
+
+        # Seleciona unidade técnica hci
         element = driver.find_element(By.XPATH, '//*[@id="demo-simple-select"]')
         element.click()
-        element = driver.find_element(By.XPATH, f'//*[@id="menu-"]/div[3]/ul/li[{unidadeTecnica}]') 
+        element = driver.find_element(By.XPATH, f'//*[@id="menu-"]/div[3]/ul/li[{unidadeTecnica}]')
         element.click()
-        #Seleciona inputtextfield e envia nome de local
+
+        # Seleciona input text field e envia nome do local
         element = driver.find_element(By.XPATH, '//*[@id="outlined-basic"]')
         element.send_keys(nomeLocal)
         time.sleep(2)
-        #Salva novo local
-        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/div[2]/div[2]/button[1]') 
-        time.sleep(2)
+
+        # Salva novo local
+        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/div[2]/div[2]/button[1]')
+        time.sleep(1)
         element.click()
-        time.sleep(2)
-        element.find_element(By.CLASS_NAME, 'MuiTypography-root MuiTypography-h2 css-qu1qus')
-        print(f'Tag name: {element.tag_name}') 
-        print(f'Text name: {element.text}')
-        print(f'Role: {element.aria_role}') 
-        if (element.text == 'Selecione uma Unidade e informe um nome para o Local!'):
-            return False
-        else:
-            return True
-    except Exception as e: 
+        time.sleep(1)
+        return True
+
+    except Exception as e:
+        print(e)
         return False
-    
-incluirLocal('600028', '123', '2', ' ')
--- OuvidTeste.py
+++ OuvidTeste.py
@@ -1,35 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import IncluirLocal
+import ouvidoria_test
 import time
 import unittest
 
-
-
-
 class OuvidTeste(unittest.TestCase):
-    
-    def test_incluirLocal(self):
-        result = IncluirLocal.incluirLocal('600028', '123', 2, 'Local CT1' )
-        self.assertTrue
-        
-    def test_incluirLocalTypo(self):
-        result = IncluirLocal.incluirLocal('600028', '123', 2, ' ' )
-        self.assertFalse
-        
-    def test_incluirLocalSemNome(self):
-        result = result = IncluirLocal.incluirLocal('600028', '123', 2, '' )
-        self.assertFalse
-        
-    def test_incluirLocalSemUndTec(self):
-        result = result = IncluirLocal.incluirLocal('600028', '123', 1, 'Local CT4' )
-        self.asserTrue
-        
-    def test_incluirLocalInfinito(self):
-        result = result = IncluirLocal.incluirLocal('600028', '123', 3, 'Local CT5 Local CT5 Local CT5 Local CT5 Local CT5 Local CT5 Local CT5 Local CT5 Local CT5' )
-        self.assertFalse    
-        
-        
-if __name__ == '__main__':
-    unittest.main()
+
+    # Happy path
+    def CT0(self):
+        result = ouvidoria_test.incluirLocal('601160', '601160', 2, 'LocalCT1')
+        self.assertTrue(result)
+
+    # Errar senha
+    def CT1(self):
+        result = ouvidoria_test.incluirLocal('601160', '601160', 2, 'LocalCT1')
+        self.assertFalse(result)
+
+    # Errar login
+    def CT2(self):
+        result = ouvidoria_test.incluirLocal('601160', '601160', 2, 'LocalCT1')
+        self.assertFalse(result)
+
+    # Incluir local com nome vazio
+    def CT3(self):
+        result = ouvidoria_test.incluirLocal('601160', '601160', 2, '')
+        self.assertFalse(result)
+
+    # Incluir local sem nome
+    def CT4(self):
+        result = result = ouvidoria_test.incluirLocal('601160', '601160', 2, '')
+        self.assertFalse(result)
+
+    # Incluir local sem unidade técnica
+    def CT5(self):
+        result = result = ouvidoria_test.incluirLocal('601160', '601160', 1, 'LocalCT4')
+        self.assertFalse(result)
+
+    # Incluir local com unidade técnica fora do dropList
+    def CT6(self):
+        result = result = ouvidoria_test.incluirLocal('601160', '601160', 7, 'LocalCT4')
+        self.assertFalse(result)
+
+    # Incluir local com mais de 20 caracteres
+    def CT7(self):
+        result = result = ouvidoria_test.incluirLocal('601160', '601160', 3, 'LocalCT7LocalCT7LocalCT7LocalCT7LocalCT7LocalCT7LocalCT7LocalCT7')
+        self.assertTrue(result)
+
+    # Incluir local com caracteres especiais
+    def CT8(self):
+        result = result = ouvidoria_test.incluirLocal('601160', '601160', 3, 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ')
+        self.assertTrue(result)
+
+    if __name__ == '__main__':
+        unittest.main()
