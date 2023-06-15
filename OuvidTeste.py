40f20c82b1206c9e29d888a19f79604cde365c22
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2023-06-15 16:18:51 -0300

    Criação de método para inclusão de respostas

--- OuvidTeste.py
+++ OuvidTeste.py
@@ -6,37 +6,45 @@ import time
 from selenium import webdriver
 
 class OuvidTest(unittest.TestCase):
+    
     # Happy path
-    def test_CT0(self):
-        result = ouvidoria_test.incluirLocal('600028', '123', 2, 'LocalCT1')
+    def test_happyPath(self):
+        result = ouvidoria_test.incluirLocal('paulo.otero', '123', 2, 'LocalCT1')
         self.assertTrue(result)
+        
     # Errar senha
-    def test_CT1(self):
-        result = ouvidoria_test.incluirLocal('600028', '12', 2, 'LocalCT1')
+    def test_errarSenha(self):
+        result = ouvidoria_test.Login('paulo.otero', '12')
         self.assertFalse(result)
+        
     # Errar login
-    def test_CT2(self):
-        result = ouvidoria_test.incluirLocal('6000228', '123', 2, 'LocalCT1')
+    def test_errarLogin(self):
+        result = ouvidoria_test.Login('paulo.otero', '123')
         self.assertFalse(result)
+        
     # Incluir local com nome vazio
-    def test_CT3(self):
-        result = ouvidoria_test.incluirLocal('600028', '123', 2, ' ')
+    def test_nomeEspaco(self):
+        result = ouvidoria_test.incluirLocal('paulo.otero', '123', 2, ' ')
         self.assertFalse(result)
+        
     # Incluir local sem nome
-    def test_CT4(self):
-        result =  ouvidoria_test.incluirLocal('600028', '123', 2, '')
+    def test_nomeVazio(self):
+        result =  ouvidoria_test.incluirLocal('paulo.otero', '123', 2, '')
         self.assertFalse(result)
+        
     # Incluir local sem unidade técnica
-    def test_CT5(self):
-        result =  ouvidoria_test.incluirLocal('600028', '123', 1, 'LocalCT4')
+    def test_semUnidadetécnica(self):
+        result =  ouvidoria_test.incluirLocal('paulo.otero', '123', 1, 'LocalCT4')
         self.assertFalse(result)
+        
     # Incluir local com unidade técnica fora do dropList
-    def test_CT6(self):
-        result =  ouvidoria_test.incluirLocal('600028', '123', 7, 'LocalCT4')
+    def test_foraDroplist(self):
+        result =  ouvidoria_test.incluirLocal('paulo.otero', '123', 7, 'LocalCT4')
         self.assertFalse(result)
+        
     # Incluir local com caracteres especiais e acima de 20 caracteres
-    def test_CT8(self):
-        result = ouvidoria_test.incluirLocal('600028', '123', 3, 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåçèéêëìíîïðñòóôõö÷øùúûüýþÿ')
+    def test_estouroCaracteres(self):
+        result = ouvidoria_test.incluirLocal('paulo.otero', '123', 3, 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåçèéêëìíîïðñòóôõö÷øùúûüýþÿ')
         self.assertTrue(result)
         
         
--- ouvidoria_test.py
+++ ouvidoria_test.py
@@ -2,13 +2,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium import webdriver
+from random import randint
 
 def incluirLocal(_username, _password, unidadeTecnica, nomeLocal):
     try:
         # Instancia webdriver e chama link de gestor
         driver = webdriver.Chrome()
         driver.implicitly_wait(10)
-        driver.get("https://haishc2.famema.br")
+        driver.get("https://g1.globo.com")
         time.sleep(2)
 
         # Entra passando credenciais
@@ -18,17 +19,17 @@ def incluirLocal(_username, _password, unidadeTecnica, nomeLocal):
         element.send_keys(_password)
         time.sleep(2)
 
-        element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/form/button')
-        element.send_keys(Keys.ENTER)
+        element = driver.find_element(By.XPATH, '/html/body/div/div/form/button')
+        element.click()
         time.sleep(5)
 
         # Clica no botão incluir novo local
-        element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/main/div[2]/div[1]/button')
+        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[2]/div[1]/button')
         element.click()
         time.sleep(2)
 
         # Seleciona unidade técnica hci
-        element = driver.find_element(By.XPATH, '//*[@id="demo-simple-select"]')
+        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[2]/div[2]/div[1]/div/div')
         element.click()
         element = driver.find_element(By.XPATH, f'//*[@id="menu-"]/div[3]/ul/li[{unidadeTecnica}]')
         element.click()
@@ -39,10 +40,12 @@ def incluirLocal(_username, _password, unidadeTecnica, nomeLocal):
         time.sleep(2)
 
         # Salva novo local
-        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/div[2]/div[2]/button[1]')
+        element = driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[2]/div[2]/button[1]')
         time.sleep(1)
         element.click()
         time.sleep(1)
+        alert = driver.find_element(By.CLASS_NAME, 'MuiAlert-message css-1xsto0d')
+        print(alert.text)
         return True
     except:
         return False
@@ -68,3 +71,40 @@ def Login(_username, _password, unidadeTecnica, nomeLocal):
             return False
     except Exception as e: 
         return False
+    
+def inserirRespostas(_local, _nome, _telefone):
+     
+    try:
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)
+        driver.get(f"https://cmdbackendtestehc.famema.br/{_local}")
+        time.sleep(1)
+        
+        #Se identifica
+        driver.find_element(By.XPATH, '/html/body/div/div/div/input[1]').send_keys(_nome)
+        driver.find_element(By.XPATH, '/html/body/div/div/div/input[2]').send_keys(_telefone)
+        driver.find_element(By.XPATH, '/html/body/div/div/div/img[2]').click()
+        time.sleep(1)
+        
+        #Clica nas respostas
+        for i in range (10):
+            try:
+                _avaliacao = randint(1,5)
+                element = driver.find_element(By.XPATH, f'/html/body/div/div/div/div[{i+1}]/div/img[{_avaliacao}]')
+                element.click()
+            except:
+                pass    
+        driver.find_element(By.XPATH, '/html/body/div/div/div/button').click()
+        time.sleep(3)
+        return True
+    except:
+        return False
+    
+    
+for i in range (250):
+    inserirRespostas(161, '', '')
+    print(f'Inclui a resposta nº{i} no local 161')
+    inserirRespostas(162, '', '')
+    print(f'Inclui a resposta nº{i} no local 162')
+    inserirRespostas(163, '', '')
+    print(f'Inclui a resposta nº{i} no local 163')
